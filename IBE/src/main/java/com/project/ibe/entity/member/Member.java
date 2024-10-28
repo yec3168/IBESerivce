92052d1d07a75c1d9b9c741455a07ba6a66027d8
kty <kty3312@gmail.com>
2024-10-28 10:53:17 +0900

    test

--- IBE/src/main/java/com/project/ibe/entity/member/Member.java
+++ IBE/src/main/java/com/project/ibe/entity/member/Member.java
@@ -3,6 +3,8 @@ package com.project.ibe.entity.member;
 import com.project.ibe.entity.common.Bank;
 import com.project.ibe.entity.common.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,31 +21,55 @@ import java.time.LocalDateTime;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memberId; // pk
+    private Long memberId;              // pk 회원번호
+
     @Column(nullable = false)
-    private String memberName; // 이름
+    @Size(max = 8)
+    private String memberName;          // 이름
+
     @Column(nullable = false)
-    private String memberNickName;
-    @Column(nullable = false)
-    private String memberPhone;
-    @Column(nullable = false)
-    private String memberAddress;
-    @Column(nullable = true)
-    private String memberAddressDetail;
+    @Size(max = 10)
+    private String memberNickName;      // 닉네임
+
     @Column(nullable = false, unique = true)
-    private String memberEmail;
+    private String memberSsn;           // 주민번호
+
     @Column(nullable = false)
-    private String memberPassword;
+    private String memberPhone;         // 전화번호
+
     @Column(nullable = false)
-    private Bank memberBank; // 은행이름.
+    private String memberAddr;          // 주소
+
+    @Column(nullable = true)
+    private String memberAddrDetail;    // 상세주소
+
+    @Column(nullable = false, unique = true)
+    private String memberEmail;         // 이메일
+
+//    @Convert(converter = )
     @Column(nullable = false)
-    private String memberAccountNumber; // 환급 계좌번호
+    private String memberPassword;      // 비밀번호
+
+    @Column(nullable = false)
+    private Bank memberBank;            // 환급은행
+
+    @Column(nullable = false)
+    private String memberAccountNumber; // 환급계좌
+
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
-    private LocalDateTime entryDate;
-    @Column(nullable = false    )
-    @UpdateTimestamp
-    private LocalDateTime updateDate;
+    private LocalDateTime entryDate;    // 가입날짜
+
     @Column(nullable = false)
-    private Role role; // 권한.
+    @UpdateTimestamp
+    private LocalDateTime updateDate;   // 수정날짜
+
+    @Column(nullable = false)
+    private Role role;                  // 권한
+
+    @Column(nullable = false)
+    private Long memberPoint;           // 보유포인트
+
+    @Column(nullable = false)
+    private String memberAuthNumber;     // 인증번호
 }
