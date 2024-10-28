7312f50f27450a11042ea46cab41113a8e187ba8
fhbsy <59546995+fhbsy@users.noreply.github.com>
2024-10-28 13:05:37 +0900

    [FEAT] add files HeaderComponent.js, FooterComponent.js, LayoutComponent.js

---
+++ ibe_front/src/component/footer/FooterComponent.js
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer>
+                <h1>아이비 FOOTER</h1>
+            </footer>
+        </div>
+    );
+}
+
+export default FooterComponent;
---
+++ ibe_front/src/component/layout/LayoutComponent.js
@@ -0,0 +1,14 @@
+import FooterComponent from "../footer/FooterComponent";
+import HeaderComponent from "../header/HeaderComponent";
+
+const LayoutComponent = () =>{
+    return (
+        <div>
+            <HeaderComponent/>
+            <p>아이비 BODY</p>
+            <FooterComponent/>
+        </div>   
+    )
+}
+
+export default LayoutComponent;
