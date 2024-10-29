ac03838df6316fca631d41fd8784bf345efd3705
fhbsy <59546995+fhbsy@users.noreply.github.com>
2024-10-29 15:42:05 +0900

    [FEAT] 메인페이지 아이콘 추가

--- ibe_front/src/component/main/CategoryIconsComponent.js
+++ ibe_front/src/component/main/CategoryIconsComponent.js
@@ -7,7 +7,11 @@ import icon_outdoor from '../assets/images/main/category/icon_outdoor.png'
 import icon_toys from '../assets/images/main/category/icon_toys.png'
 
 const categoryIcons = [
-    icon_all, icon_books, icon_clothes, icon_etc, icon_outdoor, icon_toys
+    icon_all, icon_clothes, icon_toys, icon_books, icon_outdoor, icon_etc
+];
+
+const categoryName = [
+    "전체", "아동 의류", "아동 완구", "아동 도서", "외출 용품", "기타"
 ];
 
 const CategoryIconsComponent = () => {
@@ -21,7 +25,7 @@ const CategoryIconsComponent = () => {
                 <div class="row row-cols-6" id="div_categoryIcons">
                     {categoryIcons.map((item, idx) => { 
                         return (
-                            <div class="col" id="div_col">
+                            <div class="col" id="div_iconCol">
                                 <img id="img_categoryIcons" src={item} alt="categoryIcon" />
                             </div>
                         )
@@ -32,24 +36,13 @@ const CategoryIconsComponent = () => {
             {/* 카테고리 icon 메뉴바 */}
             <div class="container" id="div_allIcons">
                 <div class="row row-cols-6" id="div_categoryName">
-                    <div class="col" id="div_col">
-                        <p>카테고리명</p>
-                    </div>
-                    <div class="col" id="div_col">
-                        <p>카테고리명</p>
-                    </div>
-                    <div class="col" id="div_col">
-                        <p>카테고리명</p>
-                    </div>
-                    <div class="col" id="div_col">
-                        <p>카테고리명</p>
-                    </div>
-                    <div class="col" id="div_col">
-                        <p>카테고리명</p>
-                    </div>
-                    <div class="col" id="div_col">
-                        <p>카테고리명</p>
-                    </div>
+                    {categoryName.map((item, idx) => { 
+                        return (
+                            <div class="col" id="div_nameCol">
+                                <div id="div_nameTxt">{item}</div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </>
