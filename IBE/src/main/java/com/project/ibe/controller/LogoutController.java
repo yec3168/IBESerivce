5309f06a8e8ff041ee135992cc2d800f91ff19ee
kty <kty3312@gmail.com>
2024-10-29 14:55:16 +0900

    [FIX] controller 경로수정(/api), 불필요한 클래스 삭제

--- IBE/src/main/java/com/project/ibe/controller/LogoutController.java
+++
@@ -1,5 +0,0 @@
-package com.project.ibe.controller;
-
-public class LogoutController {
-
-}
--- IBE/src/main/java/com/project/ibe/dto/RegisterDTO.java
+++
@@ -1,9 +0,0 @@
-package com.project.ibe.dto;
-
-import lombok.Data;
-
-@Data
-public class RegisterDTO {
-    private String memberEmail;
-    private String memberPassword;
-}
