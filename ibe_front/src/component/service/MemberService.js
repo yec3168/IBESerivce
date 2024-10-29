1d73d6f83fa8f7be30aa38ceae7b81beee7628ef
yec3168 <85733545+yec3168@users.noreply.github.com>
2024-10-29 14:53:30 +0900

    [FEAT} memberService 로그인, 이메일 중복확인 추가.

--- ibe_front/src/component/service/MemberService.js
+++ ibe_front/src/component/service/MemberService.js
@@ -6,5 +6,12 @@ const REST_API_URL = `${REST_API_BASE_URL}/members`
 
 
 // 회원 등록
-export const saveMember = (member) => axios.post( `${REST_API_URL}/signup`, member )
+export const saveMember = (memberFrom) => axios.post( `${REST_API_URL}/signup`, memberFrom )
 
+
+// 로그인
+export const login = (loginMember) => axios.post(`${REST_API_URL}/signin`, loginMember);
+
+
+// 이메일 중복확인
+export const checkEmail = (memberEmail) => axios.get(`${REST_API_URL}/signup/${memberEmail}`)
