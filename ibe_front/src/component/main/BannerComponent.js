61aca99325a90219fa28aa2787c1b79119870cc7
fhbsy <59546995+fhbsy@users.noreply.github.com>
2024-10-29 13:23:29 +0900

    [REFACTOR] 배너 배경 이미지 변경

--- ibe_front/src/component/main/BannerComponent.js
+++ ibe_front/src/component/main/BannerComponent.js
@@ -2,10 +2,11 @@ import Carousel from 'react-bootstrap/Carousel';
 import banner_info from '../assets/images/main/banner/banner_info.png'
 import banner_trust from '../assets/images/main/banner/banner_trust.png'
 import banner_coin from '../assets/images/main/banner/banner_coin.png'
+import banner_teddy from '../assets/images/main/banner/banner_teddy.png'
 import './MainComponent.css';
 
 const banner_imgs = [
-    banner_info, banner_trust, banner_coin
+    banner_teddy, banner_info, banner_trust, banner_coin
 ];
 
 const BannerComponent = () => {
