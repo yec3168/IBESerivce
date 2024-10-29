bcc06a543a995b76931d6f745f74261db6783d4e
fhbsy <59546995+fhbsy@users.noreply.github.com>
2024-10-30 00:39:23 +0900

    [STYLE] 배너 이미지 변경, 배너 속도 변경

--- ibe_front/src/component/main/BannerComponent.js
+++ ibe_front/src/component/main/BannerComponent.js
@@ -2,11 +2,11 @@ import Carousel from 'react-bootstrap/Carousel';
 import banner_info from '../assets/images/main/banner/banner_info.png'
 import banner_trust from '../assets/images/main/banner/banner_trust.png'
 import banner_coin from '../assets/images/main/banner/banner_coin.png'
-import banner_teddy from '../assets/images/main/banner/banner_teddy.png'
+import banner_frog from '../assets/images/main/banner/banner_frog_trim.jpg'
 import './Main.css';
 
 const bannerImgs = [
-    banner_teddy, banner_info, banner_trust, banner_coin
+    banner_frog, banner_info, banner_trust, banner_coin
 ];
 
 const BannerComponent = () => {
@@ -14,7 +14,7 @@ const BannerComponent = () => {
     <Carousel>
         {bannerImgs.map((item, idx) => { 
             return (
-                <Carousel.Item interval={2000}>
+                <Carousel.Item interval={2500}>
                     <img id="img_carousel" src={item} alt="banner" />
                 </Carousel.Item>
             )
