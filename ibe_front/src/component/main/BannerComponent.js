3173bcd81139c76fb26c8de513834dd0d8308cf4
tngml <sonsu4460@gmail.com>
2024-10-30 13:04:03 +0900

    main banner 사진수정

--- ibe_front/src/component/main/BannerComponent.js
+++ ibe_front/src/component/main/BannerComponent.js
@@ -1,26 +1,24 @@
-import Carousel from 'react-bootstrap/Carousel';
-import banner_info from '../assets/images/main/banner/banner_info.png'
-import banner_trust from '../assets/images/main/banner/banner_trust.png'
-import banner_coin from '../assets/images/main/banner/banner_coin.png'
-import banner_frog from '../assets/images/main/banner/banner_frog_trim.jpg'
-import './Main.css';
+import Carousel from "react-bootstrap/Carousel";
+import banner_info1 from "../assets/images/main/banner/banner_info1.png";
+import banner_trust1 from "../assets/images/main/banner/banner_trust1.png";
+import banner_coin from "../assets/images/main/banner/banner_coin.png";
+import banner_frog from "../assets/images/main/banner/banner_frog_trim.jpg";
+import "./Main.css";
 
-const bannerImgs = [
-    banner_frog, banner_info, banner_trust, banner_coin
-];
+const bannerImgs = [banner_frog, banner_info1, banner_trust1, banner_coin];
 
 const BannerComponent = () => {
   return (
     <Carousel>
-        {bannerImgs.map((item, idx) => { 
-            return (
-                <Carousel.Item interval={2500}>
-                    <img id="img_carousel" src={item} alt="banner" />
-                </Carousel.Item>
-            )
-        })}
+      {bannerImgs.map((item, idx) => {
+        return (
+          <Carousel.Item interval={2500}>
+            <img id="img_carousel" src={item} alt="banner" />
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
-}
+};
 
 export default BannerComponent;
