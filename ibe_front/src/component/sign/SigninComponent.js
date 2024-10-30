b774af4559409c108f5bf303d01aaf0c8d68f119
yec3168 <85733545+yec3168@users.noreply.github.com>
2024-10-30 09:33:46 +0900

    [REFACTOR] 로그인 try-catch 수정.

--- ibe_front/src/component/sign/SigninComponent.js
+++ ibe_front/src/component/sign/SigninComponent.js
@@ -27,26 +27,37 @@ function SignInComponent(){
             memberEmail: memberEmail,
             memberPassword: memberPassword
         }
-      
-        login(memberForm).then(
-            response => {
-                console.log(response.data)
-                try{
-                    if(response.data.status === "200"){
-                        alert("로그인 성공!")
-                        window.location.href ="/"
-                        setIsEmpty(false)
-                    }
-                } catch(error){
-                    if(error.response.status === 404){
-                        setIsEmpty(true)
-                        return false;
-                    }
-                   
-                }
-            
-            }
-        )
+        login(memberForm)
+            .then(response => {
+            console.log(response.data);
+            if (response.data.status === "200") {
+                alert("로그인 성공!");
+                window.location.href = "/";
+                setIsEmpty(false);
+            }   
+        })
+        .catch(error => {
+            console.error("로그인 실패:", error); // 에러 로그 확인
+            setIsEmpty(true);
+        });
+        // try{
+        //     login(memberForm).then(
+        //         response =>{
+        //             console.log(response.data)
+        //             if(response.data.status === "200"){
+        //                 alert("로그인 성공!")
+        //                 window.location.href ="/"
+        //                 setIsEmpty(false)
+        //             }
+        //         }
+        //     )
+        // }catch(error){
+        //     if(error.response.status === 404){
+        //         setIsEmpty(true)
+        //         return false;
+        //     }
+        // }
+
     }
 
     return(
