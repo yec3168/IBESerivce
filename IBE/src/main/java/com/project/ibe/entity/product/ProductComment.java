6dbd88d34ed6bceae7ca625adb6428a1e2278c19
fhbsy <59546995+fhbsy@users.noreply.github.com>
2024-11-02 01:59:05 +0900

    [FEAT] ProductComment, ProductReply entity 생성

---
+++ IBE/src/main/java/com/project/ibe/entity/product/ProductComment.java
@@ -0,0 +1,35 @@
+package com.project.ibe.entity.product;
+
+import com.project.ibe.entity.member.Member;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_comment")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_comment_id")
+    private Long productCommentId;
+
+    @Column(nullable = false)
+    private String productCommentContent;
+
+    @Column(nullable = false)
+    private LocalDateTime productCommentCreatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
---
+++ IBE/src/main/java/com/project/ibe/entity/product/ProductReply.java
@@ -0,0 +1,41 @@
+package com.project.ibe.entity.product;
+
+import com.project.ibe.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.mapping.ToOne;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_reply")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductReply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_reply_id")
+    private Long productReplyId;
+
+    @Column(nullable = false)
+    private String productReplyContent;
+
+    @Column(nullable = false)
+    private LocalDateTime productReplyCreatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "product_comment_id")
+    private ProductComment productComment;
+}
