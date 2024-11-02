b98af4b3c4c7b7016ce1c0021b05a0a4ae516765
gh934 <gh934@naver.com>
2024-11-02 21:02:47 +0900

    [FEAT] 계좌이체 추가

---
+++ IBE/src/main/java/com/project/ibe/dto/points/NhRequest.java
@@ -0,0 +1,17 @@
+package com.example.pay.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NhRequest {
+
+private String  bankName;
+private String  bankAccountNumber;
+private String  memberPoint;
+
+}
+
--- ibe_front/src/component/mypage/MypagePointChargeResultComponent.js
+++ ibe_front/src/component/mypage/MypagePointChargeResultComponent.js
@@ -3,10 +3,12 @@ import { useLocation } from "react-router-dom";
 const MypagePointChargeResult = (Props)=>{
     const location = useLocation();
     console.log(location.state);
+    let name = location.state.partner_user_id;
+    let points = location.state.item_name;
     return(
         <>
-        {location.state.partner_user_id}님  <br/>
-        {location.state.item_name} 충전 완료되었습니다. <br/>
+        {name}님  <br/>
+        {points} 충전 완료되었습니다. <br/>
         <button onClick={()=>window.close()}>확인</button>
         
         </>
