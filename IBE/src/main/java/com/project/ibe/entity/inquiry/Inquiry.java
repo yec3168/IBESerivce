67b6149c0037300bfd410e138b28cc1992aee8ed
fhbsy <59546995+fhbsy@users.noreply.github.com>
2024-11-03 17:38:01 +0900

    [FEAT] inquiry, inquiryAnswer Entity 추가, InquiryCategory enum 클래스 추가

---
+++ IBE/src/main/java/com/project/ibe/entity/inquiry/Inquiry.java
@@ -0,0 +1,42 @@
+package com.project.ibe.entity.inquiry;
+
+import com.project.ibe.entity.common.InquiryCategory;
+import com.project.ibe.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+@Setter
+@Table(name = "inquiry")
+public class Inquiry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inquiry_id")
+    private Long inquiryId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InquiryCategory inquiryCategory;
+
+    @Column(nullable = false)
+    private String inquiryTitle;
+
+    @Column(nullable = false)
+    private String inquiryContent;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime inquiryCreatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
