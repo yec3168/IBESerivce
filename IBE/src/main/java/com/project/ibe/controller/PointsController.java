17022958810b31976d480f49d41ed828388daa93
gh934 <gh934@naver.com>
2024-11-04 22:05:44 +0900

    [REFACTOR] 포인트 컨트롤러 수정

--- IBE/src/main/java/com/project/ibe/controller/PointsController.java
+++ IBE/src/main/java/com/project/ibe/controller/PointsController.java
@@ -2,6 +2,8 @@ package com.project.ibe.controller;
 
 import com.project.ibe.dto.member.PrincipalDTO;
 import com.project.ibe.dto.points.*;
+import com.project.ibe.entity.common.Response;
+import com.project.ibe.entity.common.ResponseCode;
 import com.project.ibe.services.points.KakaoPayService;
 import com.project.ibe.services.points.NhService;
 import lombok.RequiredArgsConstructor;
@@ -19,27 +21,34 @@ public class PointsController {
     private final NhService nhService;
 
     @PostMapping("/kakao/ready")
-    public KakaoReadyResponse kakaoReady(@RequestBody KakaoReadyRequest request
+    public Response kakaoReady(@RequestBody KakaoReadyRequest request
             ,@AuthenticationPrincipal PrincipalDTO principal) {
         log.info("kakaoController 호출");
         String name = request.getPriceName();
         int totalPrice = request.getTotalPrice();
         log.info("주문 상품 이름: " + name);
         log.info("주문 금액: " + totalPrice);
-        // 카카오 결제 준비하기
-        KakaoReadyResponse readyResponse = kakaoPayService.payReady(name, totalPrice,principal);
-        // 세션에 결제 고유번호(tid) 저장
-        log.info("결제 고유번호: " + readyResponse.getTid());
-        return readyResponse;
+        try{
+            // 카카오 결제 준비하기
+            KakaoReadyResponse readyResponse = kakaoPayService.payReady(name, totalPrice,principal);
+            log.info("결제 고유번호: " + readyResponse.getTid());
+            return  new Response(ResponseCode.SUCCESS,readyResponse, "200");
+        }catch (Exception e){
+            return  new Response(ResponseCode.FAIL, e.getMessage(), "404");
+        }
     }
 
     @GetMapping("/kakao/completed")
-    public ApproveResponse kakaoCompleted(@RequestParam("pg_token") String pgToken, @RequestParam("tid") String tid) {
+    public Response kakaoCompleted(@RequestParam("pg_token") String pgToken, @RequestParam("tid") String tid) {
        log.info("kakaoCompletedController 호출");
         log.info("결제승인 요청을 인증하는 토큰: " + pgToken);
         log.info("결제 고유번호: " + tid);
         // 카카오 결제 요청하기
-        return kakaoPayService.payApprove(tid, pgToken);
+        try{
+            return  new Response(ResponseCode.SUCCESS,kakaoPayService.payApprove(tid, pgToken), "200");
+        }catch (Exception e){
+            return  new Response(ResponseCode.FAIL, e.getMessage(), "404");
+        }
     }
 
     @PostMapping("/nh/transfer")
