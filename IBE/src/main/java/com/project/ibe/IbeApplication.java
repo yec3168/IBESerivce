b9b307b96d0cef67d012bc90564cfc6422e92067
yec3168 <85733545+yec3168@users.noreply.github.com>
2024-11-05 10:37:03 +0900

    [FEAT] 소셜 경로, 아이디 추가.

--- IBE/src/main/java/com/project/ibe/IbeApplication.java
+++ IBE/src/main/java/com/project/ibe/IbeApplication.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IbeApplication {
@@ -15,6 +16,11 @@ public class IbeApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
