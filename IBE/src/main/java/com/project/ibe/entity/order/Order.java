d776153c0fd33da99b017a0d73e75ca30fb26429
yec3168 <85733545+yec3168@users.noreply.github.com>
2024-11-07 10:31:43 +0900

    [FEAT] 구매정보저장 api 추가.

---
+++ IBE/src/main/java/com/project/ibe/entity/order/Order.java
@@ -0,0 +1,47 @@
+package com.project.ibe.entity.order;
+
+import com.project.ibe.entity.common.OrderState;
+import com.project.ibe.entity.member.Member;
+import com.project.ibe.entity.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PUBLIC) //protected
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(unique = false)
+    private OrderState orderState;
+
+    @Column(unique = false)
+    @CreationTimestamp
+    private LocalDateTime orderDate; //구매신청시간
+
+    private LocalDateTime orderDeliveryDate; //배송시작시간  -> 따로 서비스에서 .now() 써줘야함.
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product; //판매물품 -> 판매자 정보도 들어있음.
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member orderMember; // 구매자.
+
+
+    @Builder
+    public Order(OrderState orderState, Product product, Member orderMember){
+        this.orderState = orderState;
+        this.product =product;
+        this.orderMember = orderMember;
+    }
+}
---
+++ IBE/src/main/java/com/project/ibe/repository/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.project.ibe.repository.order;
+
+import com.project.ibe.entity.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
