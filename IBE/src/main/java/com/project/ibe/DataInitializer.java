264dd9a73d991964ff2f66a19d970bbe80dd3f58
kty <kty3312@gmail.com>
2024-11-07 13:58:15 +0900

    after pull

--- IBE/src/main/java/com/project/ibe/DataInitializer.java
+++ IBE/src/main/java/com/project/ibe/DataInitializer.java
@@ -21,6 +21,7 @@ public class DataInitializer {
 
     @PostConstruct
     public void init() {
+        executeSqlScript("classpath:sql/board_dummy.sql");
         executeSqlScript("classpath:sql/member_dummy.sql");
         executeSqlScript("classpath:sql/product_dummy.sql");
         executeSqlScript("classpath:sql/productImg_dummy.sql");
