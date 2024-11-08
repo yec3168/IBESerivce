7633fc1aefcc4b117c219d401bf3d4e186ff433f
gh934 <gh934@naver.com>
2024-11-08 10:39:28 +0900

    [FIX] ProductListComponent.js 의 물품 없을 시 푸터 간격 조정

--- ibe_front/src/component/product/ProductListComponent.js
+++ ibe_front/src/component/product/ProductListComponent.js
@@ -105,6 +105,13 @@ const ProductListComponent = () => {
             <i className="bi bi-exclamation-circle" style={{ fontSize: '3rem', color: 'red' }}></i>
             <h4 className="mt-2">찾으시는 검색결과가 없습니다</h4>
             <p>다른 키워드로 검색해 주세요.</p>
+            <div id="div_spacing"/>
+            <div id="div_spacing"/>
+            <div id="div_spacing"/>
+            <div id="div_spacing"/>
+            <div id="div_spacing"/>
+            <div id="div_spacing"/>
+            
          </div>
         )}
         
